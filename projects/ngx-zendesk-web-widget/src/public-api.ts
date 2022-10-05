4bec1047c9d13bbe01930ac0bee5131ee26b989f
Erich Json Fosse <erich@halloverden.no>
2022-10-05 18:21:10 +0200

    FIX: Exporting library features

--- projects/ngx-zendesk-web-widget/src/public-api.ts
+++ projects/ngx-zendesk-web-widget/src/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of ngx-zendesk-web-widget
  */
 
+export * from './lib/ngx-zendesk-web-widget.config';
+export * from './lib/ngx-zendesk-web-widget.factory';
 export * from './lib/ngx-zendesk-web-widget.service';
 export * from './lib/ngx-zendesk-web-widget.module';
+export * from './lib/ngx-zendesk-web-widget-ze-instance.interface';
+export * from './lib/ngx-zendesk-web-widget-ze-settings.interface';
